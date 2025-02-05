535a4fe985eb8ffc0a0b45743817984e6138b2f9
Pia Katleya Macalanda <130662592+PiaMacalanda@users.noreply.github.com>
2025-02-05 01:26:47 +0000

    initial commit

---
+++ activity1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "ENTER NAME:"
+read name
+
+echo "ENTER YOUR AGE:"
+read age
+
+echo "ENTER YOUR PROGRAM:"
+read program
+
+echo "Hello $name, this is your age - $age & your program is $program"
