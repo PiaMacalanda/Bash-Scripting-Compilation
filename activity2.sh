2605044d23382dd046d4767c36ece35091be96d8
Pia Katleya Macalanda <130662592+PiaMacalanda@users.noreply.github.com>
2025-02-12 01:04:49 +0000

    add: activity:3

---
+++ activity2.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+echo "ACTIVITY 3"
+echo -n "ENTER NAME:"
+read name
+
+echo -n "ENTER YOUR AGE:"
+read age
+
+echo -n "ENTER YOUR PROGRAM:"
+read program
+
+echo e- "\nThank you, $name! "You are this is your age - $age & your program is $program."
