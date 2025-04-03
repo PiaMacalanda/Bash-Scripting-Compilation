f6ff050f2951bad3b84d5cc48570b5b02a4edfd4
Pia Katleya Macalanda <130662592+PiaMacalanda@users.noreply.github.com>
2025-04-03 10:56:17 +0000

    clean and fix repo file manager

--- log_time.sh
+++
@@ -1,1 +0,0 @@
-Current Date and Time: Wed Apr  2 00:15:51 UTC 2025
--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo -n "Enter your grade"
-read grade
-
-if [[$grade -gt 60 && $grade -lt 80]]; then
-echo "Grade is F"
-fi
